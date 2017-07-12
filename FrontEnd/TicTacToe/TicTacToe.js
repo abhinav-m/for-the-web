bc000f1ed78fdad449a402c57a53e9774a67681c
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-07-12 19:36:26 +0530

    Various UI changes, BUG FIXES, changes to code , added animations and transitions, Made functionalities bug free.

--- FrontEnd/TicTacToe/TicTacToe.js
+++ FrontEnd/TicTacToe/TicTacToe.js
@@ -12,6 +12,9 @@ $(document).ready(function() {
 	var aiMoved;
 	var emptyCells;
 	var allCells;
+	var gameStarted;
+	var highLight;
+	var isWin;
 	init();
 	setClickMethods();
 });
@@ -34,7 +37,17 @@ function init() {
 	gameEnd = false;
 	playerMoved = false;
 	aiMoved  = false;
+	gameStarted = false;
+	isWin = false;
+	highLight = "";
+	allCells = ["00","01","02","10","11","12","20","21","22"];
 	makeBoard();
+	$("td").addClass("testHover whiteText");
+	clearBoard();
+}
+
+function clearBoard() {
+	$("td").text("");
 }
 
 /*Set click methods for 
@@ -42,7 +55,15 @@ function init() {
   */
 
 function setClickMethods() {
-	$("td").click(function(){ playMove(this.id);});
+	if(!gameEnd)
+	{
+	for(var i =0;i<emptyCells.length;i++)
+	{
+	$("#"+emptyCells[i]).off('click').on('click', function () {
+ 	 playMove(this.id);
+	});
+	}
+	}
 }
 
 /*This method runs on click of the
@@ -135,9 +156,18 @@ Each iteration of the loop alternates player's move by multiplying current turn 
 */
 
 function startGame() {
+	gameStarted = true;
+	if(!gameEnd)
+	{
 	checkGameEnd();
 	if(currentTurn==1&&AI==1||currentTurn==-1&&AI==0)
+	{
+	$('td').unbind('click');
 	aiMove();
+	}
+	else
+	setClickMethods();
+	}
 	
 	
 }
@@ -153,15 +183,17 @@ function playMove(id) {
 	if(currentTurn == 1)
 	{
 	$("#"+id).text("X")
-	playerMoved = true;
+	$("#"+id).animate({'fontSize':'2em'},"slow");
 	ticTacBoard[row][col] = 1;
 	}
 	else
 	{
-	$("#"+id).text("0");
+	$("#"+id).text("0");	
+	$("#"+id).animate({'fontSize':'2em'},"slow");
 	ticTacBoard[row][col] = 0;
 	}
-	$("#"+id).animate({'fontSize':'2em'},"fast");
+	$("#"+id).unbind('click');
+	$("#"+id).removeClass("testHover");
 	emptyCells.splice(emptyCells.indexOf(id),1);
 	currentTurn = currentTurn * -1;
 	startGame();
@@ -187,38 +219,51 @@ A win  = +10 score. (Maximising player).
 A win for the other player = -10 score.
 */
 
-function evaluateBoard(ticTacBoard) {
-
+function evaluateBoard() {
+    isWin = false;
+    highLight = "";
 	var score = 0;
-
 	//Check for win conditions for row
 	//and see who won in this simulation (AI/Player)
 	for (var i = 0;i< 3;i++)
-		if(ticTacBoard[i][0]==ticTacBoard[i][1]&&ticTacBoard[i][1]==ticTacBoard[i][2])
+		if(ticTacBoard[i][0]==ticTacBoard[i][1]&&ticTacBoard[i][1]==ticTacBoard[i][2]&&ticTacBoard[i][1]!=-1)
 		{
 			if(ticTacBoard[i][0]==AI)
 			score = 10;
 			else if(ticTacBoard[i][0]==player)
 			score = -10;
+			highLight = "r"+i;
+			isWin =  true;
 		}
 
 	//Check for win conditions for column
 	//and see who won in this simulation (AI/Player)
 	for (var i = 0;i< 3;i++)
-		if(ticTacBoard[0][i]==ticTacBoard[1][i]&&ticTacBoard[1][i]==ticTacBoard[2][i])
+		if(ticTacBoard[0][i]==ticTacBoard[1][i]&&ticTacBoard[1][i]==ticTacBoard[2][i]&&ticTacBoard[1][i]!=-1)
+		{
 			if(ticTacBoard[0][i]==AI)
 			score = 10;
 			else if(ticTacBoard[0][i]==player)
 			score = -10;
+			highLight = "c"+i;
+			isWin =  true;
+		}
 
 	//Check diagnol win condition and who won (AI/Player)
 	//Left diagnol OR Right diagnol win.
 	if(ticTacBoard[0][0]==ticTacBoard[1][1]&&ticTacBoard[1][1]==ticTacBoard[2][2]
-		||ticTacBoard[0][2]==ticTacBoard[1][1]&&ticTacBoard[2][0]==ticTacBoard[1][1])
-		if(ticTacBoard[1][1]==AI)
+		||ticTacBoard[0][2]==ticTacBoard[1][1]&&ticTacBoard[2][0]==ticTacBoard[1][1] && ticTacBoard[1][1]!=-1)
+	{
+		if(ticTacBoard[0][0]==ticTacBoard[1][1])
+			highLight = "d1";
+		else
+			highLight = "d2";
+			if(ticTacBoard[1][1]==AI)
 			score = 10;
 			else if(ticTacBoard[1][1]==player)
 			score = -10; 
+			isWin =  true;
+	}
 
 
 		return score;
@@ -244,7 +289,7 @@ by both player and AI. Note: AI moves are always
 considered OPTIMAL */
 
 function miniMax(depth,isMax)	{
-	var score = evaluateBoard(ticTacBoard);
+	var score = evaluateBoard();
 
 	//Break condition 1 for recursion
 	//Maximiser won
@@ -344,19 +389,123 @@ function checkGameEnd() {
 	{
 
   	$(".header").fadeOut("slow",function() {
-  $(".header").text("Draw");
-}).fadeIn("slow");
+  	$(".header").html("Boring. A Draw. <i id='resetButton' class='fa fa-undo' aria-hidden='true'></i>");
+	}).fadeIn("slow");
+	gameEnd = true;
+
 	}
-	else if(evaluateBoard(ticTacBoard)==10)
+	else if(evaluateBoard()==10)
 	{
-	 	$(".header").fadeOut("slow",function() {
-  $(".header").html("AI Wins. <span id='notif'>FATALITY.</span>");
-}).fadeIn("slow");
-
-
-
-	}
-	else if(evaluateBoard(ticTacBoard)==-10)
+ 	$(".header").fadeOut("slow",function() {
+  	$(".header").html("AI Wins. <span id='notif'>FATALITY.</span> <i id='resetButton' class='fa fa-undo' aria-hidden='true'></i>");
+  	}).fadeIn("slow");
+ 	gameEnd = true;
+ 	}
+	else if(evaluateBoard()==-10)
 	$(".header").text("This is actually a bug. Please report to game dev ASAP.");
+	
+	if(gameEnd)
+	setEndScreen();
+	
+}
 
+
+function resetGame () {
+	$(".header").fadeOut("slow",function() {
+  	$(".header").text("Tic Tac Toe");
+  	}).fadeIn("slow");
+
+	var elements = highLight.split("");
+	var num;
+	num = parseInt(elements[1]);
+  	animateWin(elements[0],num,true);
+ 
+	init();
+	setClickMethods();
+	oButton.animate({'line-height':"100px",height:"100px",width:"100px",fontSize:"1.5em",opacity:'1'},"fast");
+	oButton.show(2000);
+	xButton.animate({'line-height':"100px",height:"100px",width:"100px",fontSize:"1.5em",opacity:'1'},"fast");
+	xButton.show(2000);
+
+
+}
+
+
+function setEndScreen() {
+	$(".header").ready(function() {
+    $(".header").on ("click", "#resetButton", function () {
+        resetGame();
+    	});
+	});
+	$('td').unbind('click');
+	$("td").removeClass("testHover");
+	evaluateBoard();
+	if(isWin)
+	{
+	var elements = highLight.split("");
+	var num;
+	num = parseInt(elements[1]);
+	animateWin(elements[0],num,false);
+	}
+}
+
+
+function animateWin(el,num,reset) {
+
+	if(el=='r'||el=='c')
+	{
+	for(var i =0;i<3;i++)
+	if(el=='r')
+	{
+	if(reset==false)
+	$("#"+num+i).addClass("colorRed");
+	if(reset==true)
+	$("#"+num+i).removeClass("colorRed");
+    }
+	else
+	{
+	if(reset==false)
+	$("#"+i+num).addClass("colorRed");
+	if(reset==true)
+	$("#"+i+num).removeClass("colorRed");
+	}
+	}
+	else
+	{
+		if(num=="1")
+		{
+
+		if(reset==false)
+		{
+		$("#00").addClass("colorRed");
+		$("#11").addClass("colorRed");
+		$("#22").addClass("colorRed");
+		}
+
+		if(reset==true)
+		{
+		$("#00").removeClass("colorRed");
+		$("#11").removeClass("colorRed");
+		$("#22").removeClass("colorRed");
+		}
+
+		}
+		else
+		{
+		if(reset==false)
+		{
+		$("#02").addClass("colorRed");
+		$("#11").addClass("colorRed");
+		$("#20").addClass("colorRed");
+
+		}
+
+		if(reset==true)
+		{
+		$("#02").removeClass("colorRed");
+		$("#11").removeClass("colorRed");
+		$("#20").removeClass("colorRed");
+		}
+		}
+	}
 }
