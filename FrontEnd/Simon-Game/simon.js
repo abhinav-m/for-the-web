bfe42b46acac344d2de4228905feb01de7ed2594
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-07-17 19:24:45 +0530

    Added logic for board sounds and lighting up

--- FrontEnd/Simon-Game/simon.js
+++ FrontEnd/Simon-Game/simon.js
@@ -14,6 +14,8 @@ $(document).ready(function() {
 	var isStrict;
 	var circles;
 	var colorsAndSounds;
+	var displayMovesInterval;
+	var displayIndex;
 	init();
 	
 });
@@ -25,10 +27,10 @@ function init() {
 	circles = [];
 	isStrict = false;
 	colorsAndSounds = {
-		"yellow":["#e5e500","#ffff33","yellowSound"],
-		"green":["#00b300","#00ff00","greenSound"],
-		"red":["#b20000","#ff0000","redSound"],
-		"blue":["#000099","#0000ff","blueSound"]
+		"yellow":["#e5e500","#ffff33","audio1"],
+		"green":["#00b300","#00ff00","audio2"],
+		"red":["#b20000","#ff0000","audio3"],
+		"blue":["#000099","#0000ff","audio4"]
 	};
 
 	playButton.onclick = initialiseGame;
@@ -51,18 +53,64 @@ for(var i =1;i<=4;i++)
 
 
 changeDisplay();
-//startGame();
+startGame();
 
 }
 
 function changeDisplay() {
 	playButton.className = "fa fa-refresh hoverBlue fa-2x";
-	
 	$(".header").animate({left: '-=5200px'});
 	$(".header").css("font-size",'2.5em');
 	$(".header").text("Score:0");
 	$(".header").animate({left: '+=5200px'});
 	$("#strictAndNotif ").hide();
+		
+}
+
+
+function startGame() {
+addMove();
+playGame();
+}
+
+
+function addMove() {
+for(var i =0;i<5;i++)
+{
+var randomMove = Math.floor(Math.random()*4);
+currentMoves.push(randomMove);
+}
+
+}
+
+function playGame () {
+	//Initialise value for changing css and playing sound.
+	displayIndex = 0;
+	var currentCircle = {}, currentAudio, renderCircle;
+	var colorBright , colorDark;
+
+	displayMovesInterval = setInterval(function() {
 	
-	
+
+	currentCircle =	circles[currentMoves[displayIndex]];
+	currentAudio = document.getElementById(currentCircle["colorAndSound"][2]);
+	colorBright =  currentCircle["colorAndSound"][1];
+	colorDark = currentCircle["colorAndSound"][0];
+	renderCircle = document.getElementById(currentCircle.id);
+	renderCircle.style.backgroundColor = colorBright;
+	setTimeout(function() {
+		 	renderCircle.style.backgroundColor = colorDark;
+		 },500);
+	currentAudio.play();
+	displayIndex++;
+	if(displayIndex==currentMoves.length-1)
+	clearInterval(displayMovesInterval);
+	} ,1000);
+}
+
+
+function displayMoves() {
+
+		//break
+
 }
