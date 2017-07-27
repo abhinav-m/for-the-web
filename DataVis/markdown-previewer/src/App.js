fb34a3d00ad30e583a3263f3a42c026dff384f68
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-07-27 09:46:21 +0530

    Imported and installed marked for markdown parsing. Initial prototype ready.

--- DataVis/markdown-previewer/src/App.js
+++ DataVis/markdown-previewer/src/App.js
@@ -1,30 +1,45 @@
 import React from 'react';
 import './App.css';
 
+var marked = require('marked')
+
+
+
 class TextComponent extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = { text: setInitialState() };
+    this.update = this.update.bind(this);
+  }
+
+  update(e) {
+    this.setState({
+      text: e.target.value
+
+    })
   }
 
   render() {
     return (
       <div className="wrapper">
-        <div className="previewDiv">
-          {sanitise(this.state.text)}
+        <div className="previewDiv" >
+          <span dangerouslySetInnerHTML={sanitise(this.state.text)} ></span>
         </div>
-        <textarea className="writeArea">
+        <textarea className="writeArea" onChange={this.update}>
           {this.state.text}
         </textarea>
-      </div>
+      </div >
     );
   }
+
+
 
 }
 
 function sanitise(text) {
-  return text + " test";
+  var raw = marked(text, { sanitise: true });
+  return { __html: raw }
 }
 
 
@@ -39,7 +54,7 @@ function App() {
 }
 
 function setInitialState() {
-  return ("My name is abhinav");
+  return ("** Bold ** *Italic * \n\n [link to google](www.google.com)\n\n # HEADING 1 \n\n ## Heading 2 \n\n ~~Struck - through - my - heart~~ \n\n > This is a quote \n\n `Inlining` \n\nList \n\n* 1\n* 2\n* 3\n* 4\n");
 }
 
 
