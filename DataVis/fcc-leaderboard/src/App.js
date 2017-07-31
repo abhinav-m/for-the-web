7a4e0d49aca1c472633bedcf08f9ee863fea4ae5
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-07-31 14:26:46 +0530

    Added functionality for highlighting class button on selection of data

--- DataVis/fcc-leaderboard/src/App.js
+++ DataVis/fcc-leaderboard/src/App.js
@@ -9,22 +9,22 @@ import './App.css'
 
 const RowData = (props) => <tr className="dataRow">
   <td className="index">{props.idx}</td>
-        <td ><img className="logo" src={props.content.img}/></td>
-        <td className = "userArea">{props.content.username}</td>
-        <td className ="points">{props.content.alltime}</td>
-        <td className ="points">{props.content.recent}</td>
+  <td ><img className="logo" src={props.content.img} /></td>
+  <td className="userArea">{props.content.username}</td>
+  <td className="points">{props.content.alltime}</td>
+  <td className="points">{props.content.recent}</td>
 </tr>
 
 class TableData extends React.Component {
- 
+
   render() {
 
-   return(
-    <table className="dataTable">
-     <tbody>
-      {this.props.datashown.map( (content,i) => <RowData key={i+1} idx={i+1} content={content} />)}
-      </tbody>
-    </table>)
+    return (
+      <table className="dataTable">
+        <tbody>
+          {this.props.datashown.map((content, i) => <RowData key={i + 1} idx={i + 1} content={content} />)}
+        </tbody>
+      </table>)
   }
 }
 
@@ -33,100 +33,116 @@ class TableData extends React.Component {
 class DataSection extends React.Component {
 
   render() {
-    return(
-      <div className = "dataSection">
-        <TableData datashown={this.props.datashown}/>
-        </div>
+    return (
+      <div className="dataSection">
+        <TableData datashown={this.props.datashown} />
+      </div>
     );
   }
 }
 
 
 class HeaderToggle extends React.Component {
- 
+  constructor() {
+    super();
+    this.state = {
+      datashown: "recent"
+    }
+  }
+
+  isActive(arg) {
+
+    if (this.props.shown === arg)
+      return "selected"
+    else
+      return "toggleButtons"
+
+  }
 
   render() {
-    return(
-      <div className = "headerToggle">
-        <Button name="All Time" click={this.props.setAll}/>
-        <Button name="Past 30" click={this.props.setRecent}/>
-        </div>
+    return (
+      <div className="headerToggle">
+        <Button dClass={this.props.datashown==="all"?"selected":"toggleButtons"} name="All Time" click={this.props.setAll} />
+        <Button dClass={this.props.datashown==="recent"?"selected":"toggleButtons"} name="Past 30" click={this.props.setRecent} />
+      </div>
     );
-    }
+  }
 
 }
 
-const Button = (props) => <div onClick={props.click} className="toggleButtons"> {props.name} </div>
+const Button = (props) => <div onClick={props.click} className={props.dClass}> {props.name} </div>
+
 
 class App extends React.Component {
 
-   constructor() {
+  constructor() {
     super();
     this.state = {
       allData: [],
       recentData: [],
-      shownData:[]
+      shownData: [],
+      shown: "recent"
     }
     this.setRecent = this.setRecent.bind(this);
-     this.setAll = this.setAll.bind(this);
+    this.setAll = this.setAll.bind(this);
   }
 
 
 
   componentDidMount() {
 
-    const allURL= "https://fcctop100.herokuapp.com/api/fccusers/top/alltime";
-    const recentURL ="https://fcctop100.herokuapp.com/api/fccusers/top/recent";
-    const  that =this;
-  var xHttpReqRecent = new XMLHttpRequest();
-  var xHttpReqAll = new XMLHttpRequest();
-  xHttpReqRecent.open("GET",recentURL);
-  xHttpReqRecent.send();
+    const allURL = "https://fcctop100.herokuapp.com/api/fccusers/top/alltime";
+    const recentURL = "https://fcctop100.herokuapp.com/api/fccusers/top/recent";
+    const that = this;
+    var xHttpReqRecent = new XMLHttpRequest();
+    var xHttpReqAll = new XMLHttpRequest();
+    xHttpReqRecent.open("GET", recentURL);
+    xHttpReqRecent.send();
 
-  xHttpReqAll.open("GET",allURL);
-  xHttpReqAll.send();
+    xHttpReqAll.open("GET", allURL);
+    xHttpReqAll.send();
 
-  xHttpReqRecent.onreadystatechange = function () {
-    if(this.readyState == 4 && this.status==200)
-      {
-      var data = JSON.parse(this.responseText);
-     that.setState({recentData:data,shownData:data});
+    xHttpReqRecent.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        that.setState({ recentData: data, shownData: data });
       }
-    
-  }
-    
 
-  xHttpReqAll.onreadystatechange = function () {
-     if(xHttpReqAll.readyState == 4 && this.status==200)
-      {
-     var  data = JSON.parse(this.responseText);
-     that.setState({allData:data});
+    }
+
+
+    xHttpReqAll.onreadystatechange = function () {
+      if (xHttpReqAll.readyState == 4 && this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        that.setState({ allData: data });
       }
+    }
+
+
+
   }
 
 
-  
-  }
-   
-  
   setAll() {
     this.setState({
-      shownData:this.state.allData
+      shownData: this.state.allData,
+      shown: "all"
     })
   }
 
   setRecent() {
     this.setState({
-      shownData:this.state.recentData
+      shownData: this.state.recentData,
+      shown: "recent"
     }
     )
   }
   render() {
-    return(
-    <div className="content">
-      FCC LEADERBOARD
-      <HeaderToggle setAll={this.setAll} setRecent={this.setRecent}/>
-      <DataSection datashown={this.state.shownData}/>
+    return (
+      <div className="content">
+        FCC LEADERBOARD
+      <HeaderToggle setAll={this.setAll} setRecent={this.setRecent} datashown={this.state.shown} />
+        <DataSection datashown={this.state.shownData} />
       </div>
     )
   }
