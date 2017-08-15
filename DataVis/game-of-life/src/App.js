1b2ffc894b3dbcca9bab0c14e8cf5a3ddd30d56f
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-08-15 20:18:49 +0530

    Added dead/live cells functionality , ran tests to generate random initial boards and render it

--- DataVis/game-of-life/src/App.js
+++ DataVis/game-of-life/src/App.js
@@ -2,7 +2,7 @@ import React  from 'react';
 
 import './App.css';
 
-
+//Helper function to initialize matrix.
 const Matrix = (rows,cols) => {
   let arr = [];
   for(let i=0 ;i<rows; i++)
@@ -14,26 +14,57 @@ const Matrix = (rows,cols) => {
   return arr;
 }
 
+//Helper function to initialise state for the main component
+//when page loads. This has the value of all the live cells in 
+//the board. Returns an array 
+//WORKS PERFECTLY , commenting for testing.
+const firstGeneration = (rows,cols) => {
+  let randomRows = generateRandom(rows,rows-10);
+  let randomCols = generateRandom(cols,cols-10);
+  let liveCells = [];
+  randomRows.forEach ( v => 
+     randomCols.forEach ( k =>     liveCells.push( v+""+k ) )
+    )
+  return liveCells;
+  }
+
+//Helper function to generate random values , returns an array 
+// with the random values.
+const generateRandom = (num,length) => {
+  let randomArray = [];
+ while(randomArray.length < length) {
+   let val =  Math.floor(Math.random() * num);
+  if(randomArray.includes(val))
+    continue;
+  else
+    randomArray.push(val)
+ }
+return randomArray;
+}
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      small:Matrix(30,50),
+      small:Matrix(30,50), 
       medium:Matrix(70,50),
       large:Matrix(100,80),
-      shown:"small"
+      shown:"small",
+      liveCells: ["1425","1426","1427"]
     }
   }
+
+
+
   render() {
     return (
       <div className="App">
         <div className="wrapper">
-      {this.state[this.state.shown].map( function(r,i)  {
+      {this.state[this.state.shown].map( (r,i) =>  {
         return(
 
-      <div className = "row" key = {i}>
-      {r.map( (e,j) => <div className="cell" key={i+""+j}></div>)}
+      <div className = "row" key = {i} id={i}>
+      {r.map( (e,j) => <div className= {this.state.liveCells.includes(i+""+j)?"cell live":"cell dead"} key={i+""+j} id={i+""+j}></div>)}
       </div> 
               )
         } )
