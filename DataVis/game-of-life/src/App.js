e36570d22b63c7a2d2d34254ca9080d985226ebd
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-08-15 15:42:00 +0530

    Added base structure of project , rendering matrix of mxn size

---
+++ DataVis/game-of-life/src/App.js
@@ -0,0 +1,45 @@
+import React  from 'react';
+
+import './App.css';
+
+
+const Matrix = (rows,cols) => {
+  let arr = [];
+  for(let i=0 ;i<rows; i++)
+    {
+    arr[i] = [];
+    for(let j=0;j<cols; j++)
+      arr[i][j] = -1;
+    }
+  return arr;
+}
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      small:Matrix(30,50),
+      medium:Matrix(70,50),
+      large:Matrix(100,80),
+      shown:"small"
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+      {this.state[this.state.shown].map( function(r,i)  {
+        return(
+
+      <div className = "row" key = {i}>
+      {r.map( (e,j) => <div className="cell" key={i+""+j}></div>)}
+      </div> 
+              )
+        } )
+      }
+      </div>
+    );
+  }
+}
+
+export default App;
