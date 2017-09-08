18ceb9088914f82cba2f12e20b4096a43bdbc442
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-09-08 11:49:29 +0530

    Removing some unnecessary files

--- fileioAsync.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-var args = process.argv;
-var bufferObject = fs.readFile(args[2],lengthCallBack);
-function lengthCallBack(err,data){
-    console.log(data.toString("utf-8").split("\n").length-1);
-}
--- fileioSync.js
+++
@@ -1,6 +0,0 @@
-var fs = require('fs');
-var args = process.argv;
-var bufferObject = fs.readFileSync(args[2]);
-
-var string = bufferObject.toString();
-console.log(string.split("\n").length-1);
