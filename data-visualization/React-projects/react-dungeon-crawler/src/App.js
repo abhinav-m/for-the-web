468eba7fbbb0be4d5c8fc2bd67b8e8b1aaeb6cf2
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-09-18 13:45:52 +0530

    Added react dungeon crawler code

--- data-visualization/React-projects/react-dungeon-crawler/src/App.js
+++ data-visualization/React-projects/react-dungeon-crawler/src/App.js
@@ -6,15 +6,15 @@ const MATRIX = (rows, cols) => {
     for (let i = 0; i < rows; i++) {
         arr[i] = [];
         for (let j = 0; j < cols; j++)
-            arr[i][j] = 0;
+            arr[i][j]  = Math.floor(Math.random() * 6);
     }
     return arr;
 }
 
 const HALF_MATRIX = (matrix) => {
   let rendered = [];
-  for(let i = 4;i<matrix.length;i++)
-    rendered[i] = matrix[i];
+  for(let i = Math.floor(matrix.length/2) - 1,j=0;i<matrix.length;i++,j++)
+    rendered[j] = matrix[i];
 
   return rendered;
 }
@@ -24,21 +24,70 @@ const HALF_MATRIX = (matrix) => {
 class Game extends Component {
   constructor(props) {
     super(props);
-    this.level = MATRIX(10,30);
+    this.level = MATRIX(20,30);
+    //Render bottom half of the matrix initially,move it as character moves.
     this.rendered = HALF_MATRIX(this.level)
     this.state = {
       level: this.level,
-      board: this.rendered
+      board: this.rendered,
+      top_index: 9,
+      bottom_index:19
     }
+    this.moveChar = this.moveChar.bind(this);
   }
+
+  /* left = 37
+  up = 38
+  right = 39
+  down = 40 */
+
+ moveChar(e) {
+   let level = this.state.level;
+   let rendered = this.state.board;
+   let topIndex = this.state.top_index;
+   let bottomIndex = this.state.bottom_index;
+   var newRow;
+
+  switch(e.which) {
+    case 38: bottomIndex--;
+             topIndex--;
+             newRow = level[topIndex];
+             rendered.pop();
+             rendered.unshift(newRow);
+             break;
+   case 40: bottomIndex++;
+            topIndex++;
+            newRow = level[bottomIndex];
+            rendered.shift();
+            rendered.push(newRow);
+            break;
+
+  default: console.log('wrong key press')
+  }
+
+  this.setState({
+    board: rendered,
+    top_index:topIndex,
+    bottom_index:bottomIndex
+  });
+ }
+
+cellClass(cellType) {
+  //0 -> Unpassable terrain, 1 -> part of dungeon, 2 -> Health ,3 -> enemy ,4 -> weapon,5-> next level entrance.
+  const cells = ['cell','cell dungeon','cell health','cell enemy','cell weapon','cell nextlevel'];
+  return cells[cellType];
+
+}
+
+
   render() {
     return (
-      <div className="wrapper">
+      <div className="wrapper" onKeyDown = {this.moveChar} tabIndex='0'>
         {this.state.board.map ( (r,i) => {
           return (<div className='row'
                     key={i}
                     id={i}>
-          {r.map( (v,j) => <div className = 'cell' key ={i+','+j} id ={i+','+j}> </div>)}
+          {r.map( (v,j) => <div className = { this.cellClass(this.state.board[i][j])  } key ={i+','+j} id ={i+','+j}> </div>)}
         </div>) } ) }
       </div>
     );
