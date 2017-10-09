0ce7f7392bfcd001c40bfe912d3e15603cd725de
Abhinav Mishra <abhinav.thinktank@gmail.com>
2017-10-09 23:40:06 +0530

    changed stuff

--- data-visualization/React-projects/react-dungeon-crawler/src/App.js
+++ data-visualization/React-projects/react-dungeon-crawler/src/App.js
@@ -163,7 +163,7 @@ class Game extends Component {
       revealed:this.revealed,
       weapon:0,
       health:100,
-      levelNum:-1,
+      levelNum:1,
     }
     this.moveChar = this.moveChar.bind(this);
   }
