71ab6d7a65737a72ceb72fe74361ebac7d0c7c76
Abhinav Mishra <abhinav.thinktank@gmail.com>
2018-03-27 14:51:05 +0530

    Minor changes to signature html and css

--- data-visualization/React-projects/fcc-leaderboard/src/components/Signature.js
+++ data-visualization/React-projects/fcc-leaderboard/src/components/Signature.js
@@ -6,10 +6,22 @@ const Signature = () => (
       <i className="fa fa-heart" aria-hidden="true" />
     </p>
     <p>
-      <a href="https://github.com/abhinav-thinktank">Abhinav Mishra</a>
+      <a
+        href="https://github.com/abhinav-m"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Abhinav Mishra
+      </a>
     </p>
     <p>
-      <a href="https://github.com/abhinav-thinktank">अभिनव मिश्रा</a>
+      <a
+        href="https://github.com/abhinav-m"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        अभिनव मिश्रा
+      </a>
     </p>
   </div>
 );
